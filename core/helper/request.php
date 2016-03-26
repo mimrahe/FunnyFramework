7df7ffce3a203e6aaea510ed682a4da5ed77b49b
mimrahe <mimrahe@gmail.com>
2016-03-27 01:35:36 +0430

    add getHas and postHas function to request class
    getHas check get existence and post postHas for post

--- core/helper/request.php
+++ core/helper/request.php
@@ -7,18 +7,36 @@ namespace Core\Helper
 		{
 			$gets = new \stdClass();
 			foreach ($items as $item) {
-				$gets->{$item} = $_GET[$item];
+				$gets->{$item} = $_GET[$item] ?: false;
 			}
 			return $gets;
+		}
+
+		public static function getHas($items = [])
+		{
+			$has = new \stdClass();
+			foreach ($items as $item) {
+				$has->{$item} = isset($_GET[$item]);
+			}
+			return $has;
 		}
 
 		public static function post($items = [])
 		{
 			$posts = new \stdClass();
 			foreach ($items as $item) {
-				$posts->{$item} = $_POST[$item];
+				$posts->{$item} = $_POST[$item] ?: false;
 			}
 			return $posts;
+		}
+
+		public static function postHas($items = [])
+		{
+			$has = new \stdClass();
+			foreach ($items as $item) {
+				$has->{$item} = isset($_POST[$item]);
+			}
+			return $has;
 		}
 
 		public static function file($items = [])
